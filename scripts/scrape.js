5d451513f6aedd460e41f514bc91b075147db15d
thescottbob <scottmcmains@gmail.com>
2019-01-21 16:52:15 -0800

    Fixed syntax errors in scrape.js

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -12,36 +12,36 @@ var articles = [];
 // This function will scrape The Washington Post website
 var scrape = function() {
   // Scrape The Washington Post website
-  return axios.get("https://www.washingtonpost.com/").then(function(res) {
+    return axios.get("https://www.washingtonpost.com/").then(function(res) {
 
-    $("div.no-skin").each(function(i, element) {
-      // Grab the header of the article
-      var head = $(this)
-        .find("a")
-        .text()
-        .trim();
+      $("div.no-skin").each(function(i, element) {
+        // Grab the header of the article
+        var head = $(this)
+          .find("a")
+          .text()
+          .trim();
 
-      // Grab the URL of the article
-      var url = $(this)
-        .find("a")
-        .attr("href");
+        // Grab the URL of the article
+        var url = $(this)
+          .find("a")
+          .attr("href");
 
-      // Grab the article summary
-      var sum = $(this.blurb)
-        .text()
-        .trim();
+        // Grab the article summary
+        var sum = $(this.blurb)
+          .text()
+          .trim();
 
-        var dataToAdd = {
-          headline: head,
-          url: "https://www.washingtonpost.com/" + url,
-          summary: sum
-        }
+          var dataToAdd = {
+            headline: head,
+            url: url,
+            summary: sum
+          }
 
-      articles.push(dataToAdd);
-      return articles;
-      },
-    );
-},
+        articles.push(dataToAdd);
+        return articles;
+      })
+  });
+};
 
 // Export the function, so other files in our backend can use it
 module.exports = scrape;
