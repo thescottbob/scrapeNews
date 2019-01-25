a391c26ebf945947d02e87b6b36ef24fc9f049ab
thescottbob <scottmcmains@gmail.com>
2019-01-24 21:49:43 -0800

    Updated scrape.js

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -7,43 +7,47 @@ var cheerio = require("cheerio");
 
 // This function will scrape The Washington Post website
 var scrape = function() {
-  // Scrape The Washington Post website
-    return axios.get("https://www.washingtonpost.com/").then(function(res) {
+  // Scrape website
+    return axios.get("https://news.ycombinator.com/").then(function(res) {
 
     var $ = cheerio.load(res.data);
     // Make an empty array to save our article info
+    console.log('scraped data returned')
     var articles = [];
 
-      $("div.top-table-col").each(function(i, element) {
-        // Grab the header of the article
+      // This grabs the <a> element, which includes story link
+      $(".storylink").each(function(i, element) {
+        
+        // console.log('element: ', element)
+
         var head = $(this)
-          .find("a")
           .text()
           .trim();
+        console.log('headline: ', head)
 
         // Grab the URL of the article
         var url = $(this)
-          .find("a")
           .attr("href");
+        console.log('url: ', url)
 
-        // Grab the article summary
-        var sum = $(this.blurb)
-          .text()
-          .trim();
+        // try grabbing something else like comment count or something
+        // var sum = $(this)
+        //   .text()
+        //   .trim();
 
         // So long as our headline and sum and url aren't empty or undefined, do the following
-      if (head && sum && url) {
+      if (head && url) {
         // This section uses regular expressions and the trim function to tidy our headlines and summaries
         // We're removing extra lines, extra spacing, extra tabs, etc.. to increase to typographical cleanliness.
         var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
-        var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+        // var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
 
         // Initialize an object we will push to the articles array
 
         var dataToAdd = {
           headline: headNeat,
-          summary: sumNeat,
-          url: "https://www.washingtonpost.com/" + url
+          url: "https://news.ycombinator.com/" + url,
+          // summary: sumNeat,
         };
 
         articles.push(dataToAdd);
